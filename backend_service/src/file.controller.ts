935da7e211e5f7b9ac777ab77862cbc395ca9704
Yuan <yuant614@gmail.com>
2024-10-19 19:52:25 +0800

    updated interface

--- backend_service/src/file.controller.ts
+++ backend_service/src/file.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Header, Req, Res, StreamableFile} from '@nestjs/common';
+import {Controller, Get, Header, Req, Res, Query, StreamableFile} from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { join } from 'path';
 import type { Response } from 'express';
@@ -8,8 +8,8 @@ export class FileController {
   @Get('download')
   @Header('Content-Type', 'application/json')
   @Header('Content-Disposition', 'attachment; filename="package.json"')
-  getFile(@Req() req: Request, @Res({ passthrough: true }) res: Response): StreamableFile {
-    console.log(req.params['target']);
+  getFile(@Query('target') target:string, @Res({ passthrough: true }) res: Response): StreamableFile {
+    console.log(target);
     const file = createReadStream(join(process.cwd(), 'package.json'));
     res.set( 'Content-Type','text/plain');
     res.set('Content-Disposition', 'attachment; filename="package.json"');
